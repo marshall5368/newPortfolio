e8f385d4bb6c390152347b882ff3f86255c8f002
marshall5368 <136056077+marshall5368@users.noreply.github.com>
2024-04-19 16:51:44 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+
+
+function bars(){
+    const hidden = document.querySelector('.hidden')
+    if(hidden.style.display === "none"){
+        hidden.style.display = "flex"
+        
+    }
+    else{
+        hidden.style.display = "none"
+       
+    }
+    
+}
+function list(){
+    hidden.style.display = "none"
+}
