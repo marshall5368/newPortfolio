721d98bebe50eb185afc7a8d4b72ae51023aaa86
marshall5368 <136056077+marshall5368@users.noreply.github.com>
2024-05-04 15:46:23 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,17 +1,48 @@
-
+ const hidden = document.querySelector('.hidden')
 
 function bars(){
-    const hidden = document.querySelector('.hidden')
-    if(hidden.style.display === "none"){
-        hidden.style.display = "flex"
+   
+    if(hidden.style.display === "flex"){
+        hidden.style.display = "none"
         
     }
     else{
-        hidden.style.display = "none"
+        hidden.style.display = "flex"
        
     }
-    
 }
 function list(){
     hidden.style.display = "none"
+}
+
+
+
+function Darkmode(){
+    var boddy  = document.getElementById("body")
+    var titletext = document.querySelectorAll(".title")
+    var home = document.getElementById("home")
+    var about = document.getElementById("textAbout")
+    var link = document.querySelectorAll(".links a")
+    if(boddy.style.backgroundColor == "white"){
+        boddy.style.backgroundColor = "#3D3B40"
+       
+        home.style.color = "white"
+        about.style.color ="white"
+        
+        for (let i = 0; i < link.length && titletext.length; i++) {
+            link[i].style.color = "white";
+            titletext[i].style.color = "white"
+          }
+    }
+    else{
+        boddy.style.backgroundColor ="white"
+        home.style.color = "#3D3B40"
+        about.style.color ="#3D3B40"
+      
+        
+        for (let i = 0; i < link.length && titletext.length; i++) {
+            link[i].style.color = "#3D3B40";
+            titletext[i].style.color = "#3D3B40"
+          }
+    }
 }
